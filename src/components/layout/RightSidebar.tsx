c5ed280bd4285741a0f069a4ac002a0f8d1e9387
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2024-11-07 11:23:46 +0600

    added filters by category

--- src/components/layout/RightSidebar.tsx
+++ src/components/layout/RightSidebar.tsx
@@ -8,8 +8,11 @@ import useAuth from "@/stores/authSore";
 import { useQuery } from "@tanstack/react-query";
 import { SubscribeCardSkeleton } from "../skeleton/SubscribeSkeleton";
 import { ReactNode } from "react";
+import { useSearchStore } from "@/stores/searchStore";
+import { Button } from "../ui/button";
 export default function RightSidebar() {
   const { auth } = useAuth();
+  const { setFilters, filters } = useSearchStore();
 
   const { data, isLoading } = useQuery({
     queryFn: async () => await getEligibility(auth?.token as string),
@@ -17,7 +20,7 @@ export default function RightSidebar() {
     // enabled: auth && auth.user?.isVerified
   });
 
-  console.log(auth?.user)
+  console.log(auth?.user);
   let content: ReactNode | null = null;
   if (data?.data && auth?.user?.isVerified === false) {
     content = (
@@ -43,15 +46,30 @@ export default function RightSidebar() {
         <CardHeader>
           <h2 className="text-xl font-bold">Search by Tag</h2>
         </CardHeader>
-        <CardContent className="space-y-1">
+
+        <CardContent className="space-y-1 flex flex-wrap gap-2">
           {categoryList.map((category) => (
-            <div key={category} className="space-y-1">
-              <p className="font-semibold text-gray-500 cursor-pointer">
+            <div key={category} className="space-y-1 ">
+              <span
+                onClick={() =>
+                  setFilters({ ...filters, selectedCategory: category })
+                }
+                className={`font-semibold cursor-pointer ${
+                  filters.selectedCategory === category
+                    ? "bg-blue-500 px-2 py-1 rounded-md text-gray-100"
+                    : "text-gray-500"
+                } `}
+              >
                 #{category}
-              </p>
+              </span>
             </div>
           ))}
+
+     
         </CardContent>
+        <div className="px-10 pb-5">
+        <Button onClick={() => setFilters({ ...filters, selectedCategory: '' })} className="w-full">Clear</Button>
+        </div>
       </Card>
     </div>
   );
